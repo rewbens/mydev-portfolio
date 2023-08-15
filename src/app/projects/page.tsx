e5d2638e5442809a4cf7953f57c143cc15bc4ccd
Ben Rew <rewbens@googlemail.com>
2023-08-15 11:16:54 +0100

    first commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,8 +7,8 @@ export default function ProjectsPage() {
       name: "Menu Finder App",
       link: "https://github.com/rewbens/Recipe_Generator_FrontEnd",
       backend: "https://github.com/rewbens/Recipe_Generator_Back_End",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description:"This project is the culmination of all that we learnt while studying professional Software development at Codeclan." 
+      
       videoLink: "./public/video.mp4",
     },
     {
